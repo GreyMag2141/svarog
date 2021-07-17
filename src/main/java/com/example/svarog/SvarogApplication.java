9f31eb7cd0d986180883b56fb1ab20e0b84f1b0f
GreyMag2141 <grey2141@yandex.ru>
2021-07-17 17:35:53 +1000

    Init commit

---
+++ src/main/java/com/example/svarog/SvarogApplication.java
@@ -0,0 +1,13 @@
+package com.example.svarog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvarogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvarogApplication.class, args);
+    }
+
+}
