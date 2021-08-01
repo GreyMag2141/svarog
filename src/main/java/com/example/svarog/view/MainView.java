30d66ea9e5837234404c55eb44f8b0f86230ee33
Ilya Vyatkin <i.vyatkin@i-teco.ru>
2021-08-01 10:59:40 +0300

    # add-vaadin

---
+++ src/main/java/com/example/svarog/view/MainView.java
@@ -0,0 +1,16 @@
+package com.example.svarog.view;
+
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new Text("Welcome to MainView!!!!"));
+
+
+    }
+
+}
