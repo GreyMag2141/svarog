1d4e9c91cd4d0b2911c7fddf65641a186b221daa
Ilya Vyatkin <i.vyatkin@i-teco.ru>
2021-08-22 12:33:26 +0300

    # add Total

--- src/main/java/com/example/svarog/view/MainView.java
+++ src/main/java/com/example/svarog/view/MainView.java
@@ -8,7 +8,6 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -39,26 +38,42 @@ public class MainView extends VerticalLayout {
             prisonerView.setGender(e.getGender().toString());
             prisonerView.setName(e.getName());
             prisonerView.setPatronymic(e.getPatronymic());
+            prisonerView.setSername(e.getSername());
             prisonerViews.add(prisonerView);
         });
 
         grid.setItems(prisonerViews);
+        grid.addColumn(PrisonerView::getName).setHeader("Name")
+                .setFooter("Total: " + prisonerViews.size() + " people");
         grid.removeColumnByKey("id");
+
+
+//        Grid.Column<Prisoner> editorColumn = grid.addComponentColumn(prisoner -> {
+//            Button edit = new Button("Edit");
+//            edit.addClassName("edit");
+//            edit.addClickListener(e -> {
+////                editor.editItem(prisoner);
+////                firstNameField.focus();
+//            });
+//            edit.setEnabled(!editor.isOpen());
+//            editButtons.add(edit);
+//            return edit;
+//        });
         add(grid);
 
-        Select<String> labelSelect = new Select<>();
-        labelSelect.setItems("Option one", "Option two");
-        labelSelect.setLabel("Label");
-
-        Select<String> placeholderSelect = new Select<>();
-        placeholderSelect.setItems("Option one", "Option two");
-        placeholderSelect.setPlaceholder("Placeholder");
-
-        Select<String> valueSelect = new Select<>();
-        valueSelect.setItems("Value", "Option one", "Option two");
-        valueSelect.setValue("Value");
-
-        add(labelSelect, placeholderSelect, valueSelect);
+//        Select<String> labelSelect = new Select<>();
+//        labelSelect.setItems("Option one", "Option two");
+//        labelSelect.setLabel("Label");
+//
+//        Select<String> placeholderSelect = new Select<>();
+//        placeholderSelect.setItems("Option one", "Option two");
+//        placeholderSelect.setPlaceholder("Placeholder");
+//
+//        Select<String> valueSelect = new Select<>();
+//        valueSelect.setItems("Value", "Option one", "Option two");
+//        valueSelect.setValue("Value");
+//
+//        add(labelSelect, placeholderSelect, valueSelect);
 
     }
 
